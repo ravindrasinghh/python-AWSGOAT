1053640d1822855fe9b1f08db390554bff3ceadc
Ravindra Singh <ravindrasinghh.me@gmail.com>
2022-08-21 13:25:14 +0530

    Create stop_ec2.py

---
+++ stop_ec2.py
@@ -0,0 +1,13 @@
+import boto3
+AWS_REGION = "us-east-1"
+ec2 = boto3.client('ec2', region_name=AWS_REGION)
+ec2_info = ec2.describe_instances()
+
+newlist = []
+for ec2_list in ec2_info["Reservations"]:
+    for instance_list in ec2_list["Instances"]:
+        if instance_list["InstanceType"] == "t2.micro":
+            newlist.append(instance_list["InstanceId"])
+
+for i in  newlist:
+    ec2.stop_instances(InstanceIds=[i])
